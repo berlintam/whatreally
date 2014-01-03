3e31ed0f0da2d1c87a886df720a3e649315ca874
tam <tam.eastley@gmail.com>
2014-01-03 19:24:19 +0100

    Give maximum length to fact title

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -4,6 +4,6 @@ class Fact < ActiveRecord::Base
 
   accepts_nested_attributes_for :user, :references, :allow_destroy => true
 
-  validates :title, presence: true
+  validates :title,  :length => {:maximum => 140}, presence: true
   validates :description, presence: true
 end
