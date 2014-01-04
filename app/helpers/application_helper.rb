40d84f9232bb31ffeb604c2fbed9ebd2f67ea9bb
tam <tam.eastley@gmail.com>
2014-01-04 14:56:33 +0100

    Make add reference link (not working)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,17 @@ module ApplicationHelper
   def icon_minus
     '<i class="icon-minus"></i>'.html_safe
   end
+
+  def icon_plus
+    '<i class="icon-plus"></i>'.html_safe
+  end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s + "/form", :f => builder)
+    end
+    link_to_function(name, ERB::Util.h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+  end
+
 end
