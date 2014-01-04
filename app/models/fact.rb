793cd5eccfb6ec65d7e5ce821a2e3bd5583fc29e
tam <tam.eastley@gmail.com>
2014-01-04 15:24:49 +0100

    Fix title length validation for fact

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -4,6 +4,9 @@ class Fact < ActiveRecord::Base
 
   accepts_nested_attributes_for :user, :references, :allow_destroy => true
 
-  validates :title,  :length => {:maximum => 140}, presence: true
+  validates :title, presence: true
   validates :description, presence: true
+
+  validates :title, length: {maximum: 140}, allow_blank: false
+
 end
